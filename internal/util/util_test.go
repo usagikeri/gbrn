e8a859db757899a493dc803436393665a9a08ec1
Usagikeri <a.k.a.usagikeri@gmail.com>
2020-06-23 23:02:45 +0900

    add util test

--- internal/util/util_test.go
+++ internal/util/util_test.go
@@ -3,20 +3,26 @@ package util_test
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 	"reflect"
 	"testing"
 
 	"github.com/usagikeri/gbrn/internal/util"
 )
 
-func TestPwdFilesSuccess(t *testing.T) {
+func TestPwdFiles(t *testing.T) {
 	FILENAME := "testfile"
-	expect := []string{"util.go", "util_test.go"}
-	actual := util.PwdFiles(FILENAME)
+	tests := []struct {
+		files []string
+		want  bool
+	}{
+		{[]string{"testdata", "util.go", "util_test.go"}, true},
+		{[]string{"testdata", "util.go", "editor.go"}, false},
+	}
 
-	for i, _ := range expect {
-		if !reflect.DeepEqual(expect[i], actual[i]) {
-			t.Fatal("error")
+	for _, tt := range tests {
+		if got := reflect.DeepEqual(util.PwdFiles(FILENAME), tt.files); got != tt.want {
+			t.Fatalf("want = %t, but %t", tt.want, got)
 		}
 	}
 
@@ -27,20 +33,38 @@ func TestPwdFilesSuccess(t *testing.T) {
 	}()
 }
 
-func TestPwdFilesFaild(t *testing.T) {
-	FILENAME := "testfile"
-	expect := []string{"editor.go", "util.go"}
-	actual := util.PwdFiles(FILENAME)
+func TestFile2Slice(t *testing.T) {
+	path, err := os.Getwd()
+	if err != nil {
+		t.Errorf("%t", err)
+	}
+	FILENAME := filepath.Join(path, "/testdata/test.txt")
 
-	for i, _ := range expect {
-		if reflect.DeepEqual(expect[i], actual[i]) {
-			t.Fatal("error")
+	tests := []struct {
+		files []string
+		want  bool
+	}{
+		{[]string{"util.go", "util_test.go"}, true},
+		{[]string{"util.go", "editor.go"}, false},
+	}
+	for _, tt := range tests {
+		if got := reflect.DeepEqual(util.File2slice(FILENAME), tt.files); got != tt.want {
+			t.Fatalf("want = %t, but %t", tt.want, got)
 		}
 	}
+}
 
-	defer func() {
-		if err := os.Remove(FILENAME); err != nil {
-			fmt.Println(err)
+func TestCheckDuplicate(t *testing.T) {
+	tests := []struct {
+		chars []string
+		want  bool
+	}{
+		{[]string{"aaa", "bbb", "ccc"}, true},
+		{[]string{"aaa", "bbb", "aaa"}, false},
+	}
+	for _, tt := range tests {
+		if got := util.CheckDuplicate(tt.chars); got != tt.want {
+			t.Fatalf("want = %t", tt.want)
 		}
-	}()
+	}
 }
